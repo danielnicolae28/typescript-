63f99a412e5936b10674d0789f4003d795850eb6
danielnicolae3628 <danielnicolae903@gmail.com>
2024-02-21 14:24:54 +0200

    basic types

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-var a = '30';
-console.log(a);
+/// basic types
+var age = 30;
+var firstName = 'Mario';
+var isFunctional = true;
+// null & undefined
+var something;
+var undefinedVariable;
+/* we cannot assign a string to a varaible number or a boolean to
+ a string the only reassign we ca do si string to string number to number etc... */
+//age = 'test reassign' // does't work
+age = 45; // works
+// isFunctional  = undefined; // does't work
+isFunctional = false;
+console.log(age);
--- index.ts
+++ index.ts
@@ -1,2 +1,22 @@
-let a  = '30';
-console.log(a);
+/// basic types
+
+let age: number  = 30;
+let firstName: string = 'Mario';
+let isFunctional : boolean = true;
+ 
+// null & undefined
+
+let something :null;
+let undefinedVariable : undefined;
+
+/* we cannot assign a string to a varaible number or a boolean to
+ a string the only reassign we ca do si string to string number to number etc... */
+
+ //age = 'test reassign' // does't work
+age = 45; // works
+
+// isFunctional  = undefined; // does't work
+
+isFunctional = false;
+
+console.log(age);
