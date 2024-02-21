d1ee0b045f641a445cd491435382d57cc350995b
danielnicolae3628 <danielnicolae903@gmail.com>
2024-02-21 21:16:04 +0200

    new info added

--- dist/index.js
+++ dist/index.js
@@ -34,3 +34,107 @@ let user = {
 user.firstName = "235";
 user.id = 2;
 console.log(user);
+//// funcitons 
+function add(a, b) {
+    return a + b;
+}
+console.log(add(0, 2));
+const subtract = (a, b) => {
+    return a - b;
+};
+console.log(subtract(5, 10));
+function addAllNumber(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log(total);
+}
+addAllNumber([1, 23, 4, 567, 2]);
+/// any type 
+let age1; // can have any type
+// can be used on array, objects, etc...
+//ex 
+let arrNums = [true, 12, 'a'];
+let obj = {
+    name: 12,
+    age: 'Daniel'
+};
+function anyFunction(value) {
+    return value + value;
+}
+//tuples 
+// array of valeus of different types in a specific  order
+let person = ['Daniel', 12, true];
+let hsla; // color codes example for tuple
+hsla = [200, '100%', '100%', 1];
+// can be used in functions
+let xy;
+function useCoords() {
+    const lat = 100;
+    const lang = 50;
+    return [lat, lang];
+}
+///  named tuples
+let user1;
+user1 = ['peach', 40];
+const authorOne = {
+    name: 'Daniel',
+    avatar: '/img'
+};
+const newPost = {
+    title: 'My first post ',
+    body: 'something interesting',
+    tags: ['gaming', 'tech'],
+    create_at: new Date(),
+    author: authorOne
+};
+/// function interface 
+function createPost(post) {
+    console.log('Create post' + post.title + post.author.name);
+}
+console.log(createPost(newPost));
+//// interface with array
+let posts = [];
+posts.push(newPost);
+function getRandom() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b];
+}
+const userOne = {
+    name: 'Daniel', score: 75
+};
+function formatUser(user) {
+    console.log(`${user.name} has a score of ${user.score}`);
+}
+formatUser(userOne);
+////// union type
+// can combine types
+let someId; // union type operator |
+someId = 1;
+someId = '2';
+let anotherId;
+anotherId = '3rfadfadsf23';
+anotherId = 5;
+function swapId(id) {
+    if (typeof id === 'string') {
+        /// type guard
+        /// can use string methods
+        return parseInt(id);
+    }
+    else {
+        //can use number methods       
+        return id.toString();
+    }
+}
+console.log(swapId(323));
+console.log(swapId('62'));
+/// tagged type
+function logDetails(value) {
+    if (value.type === 'user') {
+        console.log(value.email);
+    }
+    if (value.type === 'person') {
+        console.log(value.firstName);
+    }
+}
+//// generics 
--- src/index.ts
+++ src/index.ts
@@ -66,3 +66,221 @@ user.id = 2;
 
 
 console.log(user);
+
+
+//// funcitons 
+
+
+function add(a: number,b : number):number{
+    return a+b;
+}
+
+console.log(add(0,2));
+
+const subtract = (a:number,b:number):number=>{
+return a-b;
+}
+
+console.log(subtract(5,10));
+
+
+function addAllNumber(items:number[]):void{
+const total =  items.reduce((a,c)=>a+c,0);
+console.log(total);
+}
+addAllNumber([1,23,4,567,2])
+
+
+/// any type 
+
+
+let age1:any; // can have any type
+
+// can be used on array, objects, etc...
+
+//ex 
+
+let arrNums: any[] = [true,12,'a'];
+let obj:{
+    name:any,
+    age:any
+} = {
+
+name:12,
+age:'Daniel'
+}
+
+
+function anyFunction(value:any):any{
+return value + value
+}
+
+//tuples 
+// array of valeus of different types in a specific  order
+
+let person :[string,number,boolean] = ['Daniel',12,true];
+
+let hsla:[number,string,string,number]; // color codes example for tuple
+hsla = [200,'100%','100%',1];
+// can be used in functions
+
+let xy :[number,number];
+
+function useCoords():[number,number]{
+    const lat = 100;
+    const lang  = 50;
+
+
+    return [lat,lang];
+}
+///  named tuples
+
+let user1:[name:string,age:number];
+
+user1 = ['peach',40];
+
+
+// interfaces
+
+interface Author{
+    name:string,
+    avatar:string,
+
+}
+
+const authorOne:Author={
+    name:'Daniel',
+    avatar:'/img'
+};
+
+interface Post{
+  title:string,
+  body:string,
+  tags:string[],
+  create_at:Date,
+  author:Author
+
+}
+
+const newPost:Post={
+    title:'My first post ',
+    body:'something interesting',
+    tags:['gaming','tech'],
+    create_at:new Date(),
+    author: authorOne
+}
+
+/// function interface 
+
+function createPost (post:Post):void{
+    console.log('Create post' + post.title+post.author.name)
+}
+
+console.log(createPost(newPost));
+//// interface with array
+
+let posts:Post[] = [];
+
+posts.push(newPost);
+
+
+//// type aliases
+/// type help to create a specific type and used to avoid code duplicity
+
+type Rgb = [number,number,number];
+
+function getRandom ():Rgb{
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+
+    return [r,g,b];
+
+}
+
+type User = {
+    name:string,
+    score:number
+}
+
+const userOne:User={
+    name:'Daniel',score:75
+}
+
+
+function formatUser(user:User):void{
+    console.log(`${user.name} has a score of ${user.score}`);
+}
+
+
+formatUser(userOne);
+
+////// union type
+// can combine types
+
+let someId:string | number; // union type operator |
+
+someId = 1;
+someId = '2';
+
+type Id = number | string;
+let anotherId: Id;
+anotherId = '3rfadfadsf23';
+
+anotherId = 5;
+
+
+//// type guards 
+type  Id2 = number | string;
+
+
+function swapId(id:Id){
+    if(typeof id === 'string' ){
+         /// type guard
+         /// can use string methods
+return parseInt(id)
+    }else{
+ //can use number methods       
+return  id.toString();
+
+    }
+
+}
+
+
+
+console.log(swapId(323));
+console.log(swapId('62'));
+
+
+///// union interfaces 
+
+
+interface UserOne {
+    type:'user'
+    userName:string,
+    email:string,
+    id:Id
+}
+
+interface Person {
+    type:'person'
+    firstName:string,
+    age:number,
+    id:Id
+}
+
+
+/// tagged type
+
+function logDetails(value: UserOne | Person): void{
+if(value.type === 'user'){
+    console.log(value.email);
+}
+if(value.type === 'person'){
+    console.log(value.firstName);
+}
+}
+
+
+//// generics 
